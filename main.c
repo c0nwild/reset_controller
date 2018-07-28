e7b892bf715778c6406c16e751f404be098722d0
conradwild <none@none.com>
2018-07-28 21:07:58 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,71 @@
+/*
+ * main.c
+ *
+ *  Created on: 18.04.2018
+ *      Author: conradwild
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#define FALSE 0
+#define TRUE 1
+
+volatile uint8_t go_to_sleep = TRUE;
+
+void start_timer() {
+	//prescaler clk/1014
+	TCCR0B |= (1 << CS02) | (1 << CS00);
+	go_to_sleep = FALSE;
+}
+
+void stop_timer() {
+	//set cs02:0 to '0'
+	TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00));
+	TCNT0 = 0;
+	go_to_sleep = TRUE;
+}
+
+ISR(INT0_vect, ISR_BLOCK) {
+	PORTB &= ~(1 << PB0);
+	start_timer();
+	reti();
+}
+
+ISR(TIM0_COMPA_vect, ISR_BLOCK) {
+	PORTB |= (1 << PB0);
+	stop_timer();
+	reti();
+}
+
+void chip_setup() {
+	cli();
+	DDRB = 0;
+	DDRB |= (1 << PB0);
+	PORTB |= (1 << PB0);
+	//pull-up
+	PORTB |= (1 << PB1);
+	//interrupt on compare match a
+	TIMSK0 |= (1 << OCIE0A);
+	OCR0A = 0xa0;
+	//int0 bei fallender Flanke
+	MCUCR |= (1 << ISC01);
+	GIMSK |= (1 << INT0);
+	sei();
+}
+
+void do_sleep() {
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+	//nur falls wir eine Flanke verpasst haben
+	sleep_mode();
+}
+
+int main(void) {
+	chip_setup();
+	while (TRUE) {
+		if (go_to_sleep == TRUE)
+			do_sleep();
+	}
+}
+
