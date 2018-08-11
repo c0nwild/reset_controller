69bf2d9f3c07c002de8f0935697b7322ff52ac0e
Conrad Wild <cwild@localhost.localdomain>
2018-08-11 09:21:38 +0200

    watchdog
    added watchdog for cyclic wakeup.

--- main.c
+++ main.c
@@ -9,8 +9,19 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
+#define WAKEUP_INTERVAL 80
+#define WAIT_CYCLES (WAKEUP_INTERVAL / 8)
+
 #define FALSE 0
 #define TRUE 1
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
 
 volatile uint8_t go_to_sleep = TRUE;
 
@@ -19,46 +30,77 @@ volatile uint8_t go_to_sleep = TRUE;
 	TCCR0B |= (1 << CS02) | (1 << CS00); \
 	go_to_sleep = FALSE;
 
-
 //set cs02:0 to '0'
 #define stop_timer \
 	TCCR0B &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); \
 	TCNT0 = 0; \
 	go_to_sleep = TRUE;
 
+static volatile unsigned cycle_count;
 
 ISR(INT0_vect, ISR_BLOCK) {
-	PORTB |= (1 << PB0);
-	start_timer;
+	sbi(PORTB, PB0);
+	start_timer
+	;
 	reti();
 }
 
 ISR(TIM0_COMPA_vect, ISR_BLOCK) {
-	PORTB &= ~(1 << PB0);
-	stop_timer;
+	cbi(PORTB, PB0);
+	stop_timer
+	;
 	reti();
+}
+
+ISR(WDT_vect) {
+	cycle_count++;
+	if (cycle_count > WAIT_CYCLES) {
+		start_timer
+		;
+	}
+}
+
+// ii: 0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=256ms, 5=500ms, 6=1s, 7=2s, 8=4s, 9=8s
+void setup_watchdog(int ii) {
+	unsigned char bb;
+	int ww;
+	if (ii > 9)
+		ii = 9;
+	bb = ii & 7;
+	if (ii > 7)
+		bb |= (1 << 5);
+	bb |= (1 << WDCE);
+	ww = bb;
+
+	cbi(MCUSR, WDRF);
+	sbi(WDTCR, WDCE);
+	sbi(WDTCR, WDE);
+	WDTCR = bb;
+	sbi(WDTCR, WDIE);
 }
 
 void chip_setup() {
 	cli();
+	//watchdog
+	setup_watchdog(9);
 	DDRB = 0;
-	DDRB |= (1 << PB0);
+	sbi(DDRB, PB0);
 	PORTB = 0;
 	//pull-up
-	PORTB |= (1 << PB1);
+	sbi(PORTB, PB1);
 	//interrupt on compare match a
-	TIMSK |= (1 << OCIE0A);
+	sbi(TIMSK, OCIE0A);
 	OCR0A = 0xa0;
 	//int0 bei fallender flanke
-	MCUCR |= (1 << ISC01);
-	GIMSK |= (1 << INT0);
+	sbi(MCUCR, ISC01);
+	sbi(GIMSK, INT0);
 	sei();
 }
 
 void do_sleep() {
 	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 	//nur falls wir eine Flanke verpasst haben
-	//sleep_mode();
+	sleep_mode();
 }
 
 int main(void) {
