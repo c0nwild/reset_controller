ceb2807e19032d473dc5b59819b6d1078e6ef817
Conrad Wild <cwild@localhost.localdomain>
2018-08-11 13:40:02 +0200

    first working version

--- main.c
+++ main.c
@@ -8,22 +8,16 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <avr/wdt.h>
 
-#define WAKEUP_INTERVAL 80
-#define WAIT_CYCLES (WAKEUP_INTERVAL / 8)
+#define WAIT_CYCLES 10
 
 #define FALSE 0
 #define TRUE 1
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-
 volatile uint8_t go_to_sleep = TRUE;
+volatile uint8_t cycle_cnt;
+volatile uint16_t int0_cnt;
 
 //prescaler clk/1014
 #define start_timer \
@@ -36,78 +30,78 @@ volatile uint8_t go_to_sleep = TRUE;
 	TCNT0 = 0; \
 	go_to_sleep = TRUE;
 
-static volatile unsigned cycle_count;
+const int CSleep2Seconds = 0b00000110;
 
 ISR(INT0_vect, ISR_BLOCK) {
-	sbi(PORTB, PB0);
+	PORTB |= (1 << PB0);
 	start_timer
 	;
-	reti();
+//	reti();
 }
 
 ISR(TIM0_COMPA_vect, ISR_BLOCK) {
-	cbi(PORTB, PB0);
+	PORTB &= ~(1 << PB0);
 	stop_timer
 	;
-	reti();
+//	reti();
 }
 
-ISR(WDT_vect) {
-	cycle_count++;
-	if (cycle_count > WAIT_CYCLES) {
+ISR(WDT_vect, ISR_BLOCK) {
+	WDTCR |= (1 << WDIE);
+	++cycle_cnt;
+	if (cycle_cnt > WAIT_CYCLES) {
+		PORTB |= (1 << PB0);
 		start_timer
 		;
+		cycle_cnt = 0;
 	}
 }
 
-// ii: 0=16ms, 1=32ms, 2=64ms, 3=128ms, 4=256ms, 5=500ms, 6=1s, 7=2s, 8=4s, 9=8s
-void setup_watchdog(int ii) {
-	unsigned char bb;
-	int ww;
-	if (ii > 9)
-		ii = 9;
-	bb = ii & 7;
-	if (ii > 7)
-		bb |= (1 << 5);
-	bb |= (1 << WDCE);
-	ww = bb;
+void start_watchdog(int interval) {
+	wdt_reset();
 
-	cbi(MCUSR, WDRF);
-	sbi(WDTCR, WDCE);
-	sbi(WDTCR, WDE);
-	WDTCR = bb;
-	sbi(WDTCR, WDIE);
+	// This order of commands is important and cannot be combined
+	MCUSR = 0;                   // reset Brown-out, Ext and PowerOn reset flags
+	WDTCR |= 0b00011000; // see docs, set (WDCE -> next set prescaler), (WDE -> Interrupt on timeout)
+	WDTCR = 0b01000000 | interval;     // set WDIE, and appropriate delay
+	ADCSRA &= ~_BV(ADEN);
 }
 
 void chip_setup() {
 	cli();
-	//watchdog
-	setup_watchdog(9);
+
 	DDRB = 0;
-	sbi(DDRB, PB0);
+	DDRB |= (1 << PB0);
 	PORTB = 0;
 	//pull-up
-	sbi(PORTB, PB1);
+	PORTB |= (1 << PB1);
 	//interrupt on compare match a
-	sbi(TIMSK, OCIE0A);
+	TIMSK |= (1 << OCIE0A);
 	OCR0A = 0xa0;
-	//int0 bei fallender flanke
-	sbi(MCUCR, ISC01);
-	sbi(GIMSK, INT0);
+//	int0 bei fallender flanke
+	MCUCR |= (1 << ISC01);
+	GIMSK |= (1 << INT0);
+
+	set_sleep_mode(SLEEP_MODE_IDLE);
+
+	start_watchdog(CSleep2Seconds);
+
 	sei();
 }
 
 void do_sleep() {
-	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-	//nur falls wir eine Flanke verpasst haben
-	sleep_mode();
+	if (go_to_sleep == TRUE) {
+		sleep_bod_disable()
+		;
+		sleep_mode()
+		;    // Now enter sleep mode
+	}
 }
 
 int main(void) {
 	chip_setup();
 	while (TRUE) {
-		if (go_to_sleep == TRUE)
-			do_sleep();
+		do_sleep();
 	}
 	return 0;
 }
