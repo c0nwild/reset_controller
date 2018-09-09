86bd739c1123695bc5f31dc42838ce459c0b722a
Conrad Wild <cwild@localhost.localdomain>
2018-09-09 11:10:18 +0200

    single INT0 event on low level with refraction time

--- main.c
+++ main.c
@@ -28,68 +28,79 @@ volatile uint16_t int0_cnt;
 	TCCR0A |= (1 << CS02) | (1 << CS00); \
 	go_to_sleep = FALSE;
 
+#define start_timer_refrac \
+	TCNT1 = 0; \
+	TCCR1B |= (1 << CS12) | (1 << CS10);
+
 //set cs02:0 to '0'
 #define stop_timer \
 	TCCR0A &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); \
 	TCNT0 = 0; \
-	go_to_sleep = TRUE;
+	go_to_sleep = TRUE; \
+	start_timer_refrac
+
+#define stop_timer_refrac \
+	TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10)); \
+	TCNT1 = 0;
 
 const int CSleep2Seconds = 0b00000110;
 
 ISR(INT0_vect, ISR_BLOCK) {
 	PORTB |= (1 << PB0);
-	start_timer(0x0a)
-	;
-//	reti();
+	start_timer(0x8a);
+	EIMSK &= ~(1 << INT0); //erst mal keine INT0 interrupts mehr
+	EIFR |= (1 << INTF0);
 }
 
-ISR(TIM0_COMPA_vect, ISR_BLOCK) {
+ISR(TIMER0_COMPA_vect, ISR_BLOCK) {
 	PORTB &= ~(1 << PB0);
 	stop_timer
 	;
-//	reti();
 }
 
-ISR(WDT_vect, ISR_BLOCK) {
-	WDTCSR |= (1 << WDIE);
-	++cycle_cnt;
-	if (cycle_cnt > WAIT_CYCLES) {
-		PORTB |= (1 << PB0);
-		start_timer(0x0a)
-		;
-		cycle_cnt = 0;
-	}
+ISR(TIMER1_COMPB_vect, ISR_BLOCK) {
+	EIMSK |= (1 << INT0);
+	stop_timer_refrac
+	;
 }
 
-void start_watchdog(int interval) {
-	wdt_reset();
+//ISR(WDT_vect, ISR_BLOCK) {
+//	WDTCSR |= (1 << WDIE);
+//	++cycle_cnt;
+//	if (cycle_cnt > WAIT_CYCLES) {
+//		PORTB |= (1 << PB0);
+//		start_timer(0x0a)
+//		;
+//		cycle_cnt = 0;
+//	}
+//}
 
-	// This order of commands is important and cannot be combined
-	MCUSR = 0;                   // reset Brown-out, Ext and PowerOn reset flags
-	WDTCSR |= 0b00011000; // see docs, set (WDCE -> next set prescaler), (WDE -> Interrupt on timeout)
-	WDTCSR = 0b01000000 | interval;     // set WDIE, and appropriate delay
-	ADCSRA &= ~_BV(ADEN);
-}
+//void start_watchdog(int interval) {
+//	wdt_reset();
+//
+//	// This order of commands is important and cannot be combined
+//	MCUSR = 0;                   // reset Brown-out, Ext and PowerOn reset flags
+//	WDTCSR |= 0b00011000; // see docs, set (WDCE -> next set prescaler), (WDE -> Interrupt on timeout)
+//	WDTCSR = 0b01000000 | interval;     // set WDIE, and appropriate delay
+//	ADCSRA &= ~_BV(ADEN);
+//}
 
 void chip_setup() {
 	cli();
 
-	DDRB = 0;
-	DDRB |= (1 << PB0);
-	PORTB = 0;
+	DDRB |= (1 << DDB0);
 	//pull-up
-	PORTB |= (1 << PB1);
+	PORTD |= (1 << PD2);
 	//interrupt on compare match a
 	TIMSK0 |= (1 << OCIE0A);
-	OCR0A = 0xa0;
+	//interrupt on compare match timer 1 a fuer refraktaerzeit
+	TIMSK1 |= (1 << OCIE1B);
+	OCR1B = 0xff;
+
 //	int0 bei fallender flanke
-	MCUCR |= (1 << ISC01);
+//	EICRA |= (1 << ISC01); TEST: Interrupt bei low level
 	EIMSK |= (1 << INT0);
-
-	set_sleep_mode(SLEEP_MODE_IDLE);
-
-//	start_watchdog(CSleep2Seconds);
-
+	EIFR |= (1 << INTF0);
 	sei();
 }
 
@@ -97,6 +108,7 @@ void do_sleep() {
 	if (go_to_sleep == TRUE) {
 		sleep_bod_disable()
 		;
+		set_sleep_mode(SLEEP_MODE_IDLE);
 		sleep_mode()
 		;    // Now enter sleep mode
 	}
@@ -105,7 +117,7 @@ void do_sleep() {
 int main(void) {
 	chip_setup();
 	while (TRUE) {
-		do_sleep();
+//		do_sleep();
 	}
 	return 0;
 }
