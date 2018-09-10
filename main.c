10a706ad7247d1b9f0f678b0a356d84b595b3b77
Conrad Wild <cwild@localhost.localdomain>
2018-09-10 13:45:09 +0200

    Sleep mode power down working.

--- main.c
+++ main.c
@@ -17,9 +17,13 @@
 #define FALSE 0
 #define TRUE 1
 
+#define INT_SRC_LB_PULSE 0x1
+#define INT_SRC_ESP_PULSE 0x2
+
 volatile uint8_t go_to_sleep = TRUE;
-volatile uint8_t cycle_cnt;
-volatile uint16_t int0_cnt;
+volatile uint8_t interrupt_src = 0;
+volatile uint8_t do_eval_interrupt = FALSE;
+//volatile uint16_t int0_cnt;
 
 //prescaler clk/1014
 #define start_timer(t_val) \
@@ -36,20 +40,19 @@ volatile uint16_t int0_cnt;
 #define stop_timer \
 	TCCR0A &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); \
 	TCNT0 = 0; \
-	go_to_sleep = TRUE; \
 	start_timer_refrac
 
 #define stop_timer_refrac \
 	TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10)); \
+	go_to_sleep = TRUE; \
 	TCNT1 = 0;
-
-const int CSleep2Seconds = 0b00000110;
 
 ISR(INT0_vect, ISR_BLOCK) {
 	PORTB |= (1 << PB0);
 	start_timer(0x8a);
 	EIMSK &= ~(1 << INT0); //erst mal keine INT0 interrupts mehr
 	EIFR |= (1 << INTF0);
+	do_eval_interrupt = TRUE;
 }
 
 ISR(TIMER0_COMPA_vect, ISR_BLOCK) {
@@ -64,33 +67,12 @@ ISR(TIMER1_COMPB_vect, ISR_BLOCK) {
 	;
 }
 
-//ISR(WDT_vect, ISR_BLOCK) {
-//	WDTCSR |= (1 << WDIE);
-//	++cycle_cnt;
-//	if (cycle_cnt > WAIT_CYCLES) {
-//		PORTB |= (1 << PB0);
-//		start_timer(0x0a)
-//		;
-//		cycle_cnt = 0;
-//	}
-//}
-
-//void start_watchdog(int interval) {
-//	wdt_reset();
-//
-//	// This order of commands is important and cannot be combined
-//	MCUSR = 0;                   // reset Brown-out, Ext and PowerOn reset flags
-//	WDTCSR |= 0b00011000; // see docs, set (WDCE -> next set prescaler), (WDE -> Interrupt on timeout)
-//	WDTCSR = 0b01000000 | interval;     // set WDIE, and appropriate delay
-//	ADCSRA &= ~_BV(ADEN);
-//}
-
 void chip_setup() {
 	cli();
 
 	DDRB |= (1 << DDB0);
 	//pull-up
-	PORTD |= (1 << PD2);
+	PORTD |= (1 << PD2) | (1 << PD3);
 	//interrupt on compare match a
 	TIMSK0 |= (1 << OCIE0A);
 	//interrupt on compare match timer 1 a fuer refraktaerzeit
@@ -99,25 +81,35 @@ void chip_setup() {
 
 //	int0 bei fallender flanke
 //	EICRA |= (1 << ISC01); TEST: Interrupt bei low level
-	EIMSK |= (1 << INT0);
+	EIMSK |= (1 << INT0) | (1 << INT1);
 	EIFR |= (1 << INTF0);
 	sei();
+}
+
+void eval_interrupt(void) {
+	interrupt_src = 0;
+	interrupt_src |= ((PIND & PIND2) ? INT_SRC_LB_PULSE : 0);
+	interrupt_src |= ((PIND & PIND3) ? INT_SRC_ESP_PULSE : 0);
+	do_eval_interrupt = FALSE;
 }
 
 void do_sleep() {
 	if (go_to_sleep == TRUE) {
 		sleep_bod_disable()
 		;
-		set_sleep_mode(SLEEP_MODE_IDLE);
+		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 		sleep_mode()
 		;    // Now enter sleep mode
 	}
+	// aufgewacht. Rausfinden, warum
+	if (do_eval_interrupt == TRUE)
+		eval_interrupt();
 }
 
 int main(void) {
 	chip_setup();
 	while (TRUE) {
-//		do_sleep();
+		do_sleep();
 	}
 	return 0;
 }
