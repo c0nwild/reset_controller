fb7923cb9bdd14770e22062f80fb41c4537ce243
Conrad Wild <cwild@localhost.localdomain>
2018-10-06 19:30:14 +0200

    i2c communication with esp8266 works

--- main.c
+++ main.c
@@ -15,60 +15,67 @@
 
 #define WAIT_CYCLES 10
 
-#define FALSE 0
-#define TRUE 1
+#define FALSE 0u
+#define TRUE 1u
 
-#define INT_SRC_LB_PULSE 0x1
-#define INT_SRC_ESP_PULSE 0x2
+//Quellen fuer INT's
+#define INT_SRC_MCP_PULSE (1 << 0) //MCP23017
+#define INT_SRC_ESP_PULSE (1 << 1) //ESP8266
+
+//Init merker
+#define STATUS_SYS_INITIALIZED (1 << 2) //Merker für erstes Init nach Poweroff
+
+//Freigabe für sleep
+#define STATUS_SYS_ALLOWRESET (1 << 3) //Freigabemerker für Reset, kommt vom ESP8266
 
 volatile uint8_t go_to_sleep = FALSE;
 volatile uint8_t interrupt_src = 0;
 volatile uint8_t do_eval_interrupt = FALSE;
+volatile uint8_t is_initialized = FALSE;
 //volatile uint16_t int0_cnt;
+
+const uint8_t i2c_addr = 0x47;
 
 //prescaler clk/1014
 #define start_timer(t_val) \
 	TCNT0 = 0; \
 	OCR0A = t_val; \
 	TCCR0A |= (1 << CS02) | (1 << CS00); \
-	go_to_sleep = FALSE;
-
-#define start_timer_refrac \
-	TCNT1 = 0; \
-	TCCR1B |= (1 << CS12) | (1 << CS10);
 
 //set cs02:0 to '0'
 #define stop_timer \
 	TCCR0A &= ~((1 << CS02) | (1 << CS01) | (1 << CS00)); \
 	TCNT0 = 0; \
-	start_timer_refrac
 
-#define stop_timer_refrac \
-	TCCR1B &= ~((1 << CS12) | (1 << CS11) | (1 << CS10)); \
-	TCNT1 = 0;
-
+//INT vom MCP23017
 ISR(INT0_vect, ISR_BLOCK) {
+	uint8_t i_src;
+	i_src = interrupt_src;
+	i_src |= INT_SRC_MCP_PULSE;
 	EIMSK &= ~(1 << INT0); //erst mal keine INT0 interrupts mehr
 	EIFR |= (1 << INTF0);
 	do_eval_interrupt = TRUE;
+	go_to_sleep = FALSE;
+	interrupt_src = i_src;
 }
 
+// INT vom ESP8266
 ISR(INT1_vect, ISR_BLOCK) {
+	uint8_t i_src;
+	i_src = interrupt_src;
+	i_src |= INT_SRC_ESP_PULSE;
 	EIMSK &= ~(1 << INT1); //erst mal keine INT1 interrupts mehr
 	EIFR |= (1 << INTF1);
 	do_eval_interrupt = TRUE;
+	go_to_sleep = FALSE;
+	interrupt_src = i_src;
 }
 
 ISR(TIMER0_COMPA_vect, ISR_BLOCK) {
 	PORTB &= ~(1 << PB0);
 	stop_timer
 	;
-}
-
-ISR(TIMER1_COMPB_vect, ISR_BLOCK) {
-	EIMSK |= (1 << INT0);
-	stop_timer_refrac
-	;
+//	EIMSK |= (1 << INT0) | (1 << INT1);
 }
 
 void chip_setup() {
@@ -79,54 +86,58 @@ void chip_setup() {
 	PORTD |= (1 << PD2) | (1 << PD3);
 	//interrupt on compare match a
 	TIMSK0 |= (1 << OCIE0A);
-	//interrupt on compare match timer 1 a fuer refraktaerzeit
-	TIMSK1 |= (1 << OCIE1B);
-	OCR1B = 0xff;
 
-//	int0/int1 bei fallender flanke
-//	EICRA |= (1 << ISC01); TEST: Interrupt bei low level
+//	Interrupt bei low level
 	EIMSK |= (1 << INT0) | (1 << INT1);
-	EIFR |= (1 << INTF0);
+	EIFR |= (1 << INTF0) | (1 << INTF1);
 	sei();
 }
 
 void eval_interrupt(volatile uint8_t *i2c_buffer) {
+	interrupt_src |= ((PIND & PIND2) ? INT_SRC_ESP_PULSE : 0);
+	i2c_buffer[0] |= interrupt_src;
 	interrupt_src = 0;
-	interrupt_src |= ((PIND & PIND2) ? INT_SRC_LB_PULSE : 0);
-	interrupt_src |= ((PIND & PIND3) ? INT_SRC_ESP_PULSE : 0);
-	i2c_buffer[0] = 0xaa;
 	do_eval_interrupt = FALSE;
 }
 
-void do_master_wakeup(void) {
-	PORTB |= (1 << PB0);
-	start_timer(0x8a);
+void do_master_wakeup(volatile uint8_t *i2c_buffer) {
+	if (i2c_buffer[0] & STATUS_SYS_ALLOWRESET) {
+		PORTB |= (1 << PB0);
+		start_timer(0x20);
+		//Verriegelung gegen erneuten reset
+		i2c_buffer[0] &= ~(STATUS_SYS_ALLOWRESET);
+	}
 }
 
-void do_sleep() {
+uint8_t allow_sleep(volatile uint8_t *i2c_buffer) {
+	uint8_t rv;
+	rv = (i2c_buffer[0] & STATUS_SYS_ALLOWRESET) ? TRUE : FALSE;
+	return rv;
+}
+
+void main_loop() {
 	if (go_to_sleep == TRUE) {
 		sleep_bod_disable()
 		;
 		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+		EIMSK |= (1 << INT0) | (1 << INT1);
 		sleep_mode()
 		;    // Now enter sleep mode
 	}
 	// aufgewacht. Rausfinden, warum und master wecken.
 	if (do_eval_interrupt == TRUE) {
 		eval_interrupt(i2cdata);    //int quelle in i2cbuffer schreiben
-		do_master_wakeup();
+		do_master_wakeup(i2cdata); //aufwecken, wenn freigabe erteilt
 	}
+	go_to_sleep = allow_sleep(i2cdata);
 }
 
 int main(void) {
 	chip_setup();
-	DDRB |= (1 << DDB0);
-	uint8_t addr = 0b10001110; //komisches Format (MSB 7:1 im TWAR enthalten Addresse!!)
+	uint8_t addr = (i2c_addr << 1); //komisches Format (MSB 7:1 im TWAR enthalten Addresse!!)
 	init_twi_slave(addr);
-	for (int n = 0; n < i2c_buffer_size; n++)
-		i2cdata[n] = 0xa;
 	while (TRUE) {
-		do_sleep();
+		main_loop();
 	}
 	return 0;
 }
--- twislave.c
+++ twislave.c
@@ -45,7 +45,6 @@ void init_twi_slave(uint8_t adr) {
  */
 ISR (TWI_vect) {
 	uint8_t data = 0;
-	PORTB |= (1 << PB0);
 	switch (TW_STATUS) //TWI-Statusregister prüfen und nötige Aktion bestimmen
 	{
 
@@ -59,46 +58,48 @@ ISR (TWI_vect) {
 
 	case TW_SR_DATA_ACK: // 0x80 Slave Receiver, ein Datenbyte wurde empfangen
 		data = TWDR; //Empfangene Daten auslesen
-		if (buffer_adr == 0xFF) //erster Zugriff, Bufferposition setzen
-				{
-			//Kontrolle ob gewünschte Adresse im erlaubten bereich
-			if (data < i2c_buffer_size + 1) {
-				buffer_adr = data; //Bufferposition wie adressiert setzen
-			} else {
-				buffer_adr = 0; //Adresse auf Null setzen. Ist das sinnvoll? TO DO!
-			}
-			TWCR_ACK
-			;// nächstes Datenbyte empfangen, ACK danach, um nächstes Byte anzufordern
-		} else //weiterer Zugriff, nachdem die Position im Buffer gesetzt wurde. NUn die Daten empfangen und speichern
-		{
+//		if (buffer_adr == 0xFF) //erster Zugriff, Bufferposition setzen
+//				{
+//			//Kontrolle ob gewünschte Adresse im erlaubten bereich
+//			if (data < i2c_buffer_size + 1) {
+//				buffer_adr = data; //Bufferposition wie adressiert setzen
+//			} else {
+//				buffer_adr = 0; //Adresse auf Null setzen. Ist das sinnvoll? TO DO!
+//			}
+//			TWCR_ACK
+//			;// nächstes Datenbyte empfangen, ACK danach, um nächstes Byte anzufordern
+//		} else //weiterer Zugriff, nachdem die Position im Buffer gesetzt wurde. NUn die Daten empfangen und speichern
+//		{
 
-			if (buffer_adr < i2c_buffer_size + 1) {
-				i2cdata[buffer_adr] = data; //Daten in Buffer schreibe
-			}
-			buffer_adr++; //Buffer-Adresse weiterzählen für nächsten Schreibzugriff
-			TWCR_ACK
-			;
-		}
+//			if (buffer_adr < i2c_buffer_size + 1) {
+		i2cdata[0] = data; //Daten in Buffer schreibe
+//			}
+//		buffer_adr++; //Buffer-Adresse weiterzählen für nächsten Schreibzugriff
+		TWCR_ACK
+		;
+//	}
 		break;
 
 //Slave transmitter
 
-	case TW_ST_SLA_ACK: //0xA8 Slave wurde im Lesemodus adressiert und hat ein ACK zurückgegeben.
+	case TW_ST_SLA_ACK:
+		//0xA8 Slave wurde im Lesemodus adressiert und hat ein ACK zurückgegeben.
 		//Hier steht kein break! Es wird also der folgende Code ebenfalls ausgeführt!
 
-	case TW_ST_DATA_ACK: //0xB8 Slave Transmitter, Daten wurden angefordert
+	case TW_ST_DATA_ACK:
+		//0xB8 Slave Transmitter, Daten wurden angefordert
 
-		if (buffer_adr == 0xFF) //zuvor keine Leseadresse angegeben!
-				{
-			buffer_adr = 0;
-		}
+//		if (buffer_adr == 0xFF) //zuvor keine Leseadresse angegeben!
+//				{
+//			buffer_adr = 0;
+//		}
 
-		if (buffer_adr < i2c_buffer_size + 1) {
-			TWDR = i2cdata[buffer_adr]; //Datenbyte senden
-			buffer_adr++; //bufferadresse für nächstes Byte weiterzählen
-		} else {
-			TWDR = 0; //Kein Daten mehr im Buffer
-		}
+//	if (buffer_adr < i2c_buffer_size + 1) {
+		TWDR = i2cdata[0]; //Datenbyte senden
+//		buffer_adr++; //bufferadresse für nächstes Byte weiterzählen
+//	} else {
+//		TWDR = 0; //Kein Daten mehr im Buffer
+//	}
 		TWCR_ACK
 		;
 		break;
@@ -106,9 +107,12 @@ ISR (TWI_vect) {
 		TWCR_ACK
 		;
 		break;
-	case TW_ST_DATA_NACK: // 0xC0 Keine Daten mehr gefordert
-	case TW_SR_DATA_NACK: // 0x88
-	case TW_ST_LAST_DATA: // 0xC8  Last data byte in TWDR has been transmitted (TWEA = “0”); ACK has been received
+	case TW_ST_DATA_NACK:
+		// 0xC0 Keine Daten mehr gefordert
+	case TW_SR_DATA_NACK:
+		// 0x88
+	case TW_ST_LAST_DATA:
+		// 0xC8  Last data byte in TWDR has been transmitted (TWEA = “0”); ACK has been received
 	default:
 		TWCR_RESET
 		;
@@ -116,7 +120,6 @@ ISR (TWI_vect) {
 		break;
 
 	} //end.switch (TW_STATUS)
-	PORTB &= ~(1 << PB0);
 } //end.ISR(TWI_vect)
 
 ////Ende von twislave.c////
